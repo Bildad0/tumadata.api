fcc5a864ff359ad97728b45a7a2dc0adcd408eb7
Bildad0 <bildadowuor@gmail.com>
2024-11-29 19:41:32 +0300

    updates and fixes

--- server.js
+++ server.js
@@ -6,16 +6,11 @@ const backupController = require('./controllers/backup.controller');
 const userController = require('./controllers/user.controller');
 const cron = require('node-cron'); // Import node-cron for scheduling tasks
 const dotenv = require("dotenv");
-
+const db = require("./models");
 
 dotenv.config();
 const app = express();
 const port = 3004;
-
-app.use(bodyParser.json());
-app.use(express.json());
-
-const db = require("./models");
 const Role = db.role;
 
 var corsOptions = {
@@ -23,17 +18,25 @@ var corsOptions = {
 }; //change to cors option after development
 
 
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to tuma data API' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
 db.sequelize.authenticate().then(() => {
   db.sequelize.sync({ force: true }).then(() => {
     initial();
   });
 });
-
-
 
 function initial() {
   Role.create({
@@ -52,17 +55,7 @@ function initial() {
   });
 }
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to tuma data API' });
-});
-
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 require('./routes/backup.routes')(app);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
